9c665f649708077efa72dd45271f0286f2512292
Chinara <azimovachinara2004@gmail.com>
2022-06-14 10:43:55 +0300

    new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+const[todos,setTodos]=useState([])
+const[todo,setTodo]=useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Todo app</h2>
+     <form>
+      <input type='text' onChange={(e)=>setTodo(e.target.value)}/>
+      <button>Add Todo</button>
+     </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
